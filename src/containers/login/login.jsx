186e849005068eced2424a5dde98d695c985f73f
guijingnan <1023252198@qq.com>
2018-08-11 18:56:11 +0800

    update master

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,6 +1,41 @@
 import React , {Component} from 'react';
+import {NavBar,List,InputItem,WhiteSpace,WingBlank,Button,Radio} from 'antd-mobile'
+import Logo from '../../components/Logo/logo'
 export default  class Login extends Component{
+    state={
+        username:'',
+        password:'',
+    };
+    handlerChange=(name,val)=>{
+        this.setState({
+            [name]:val
+        })
+    };
+    login=()=>{
+        console.log(this.state)
+
+    };
+    toRegister=()=>{
+        this.props.history.replace('/register')
+    }
     render(){
-        return (<div>login</div>)
+        return (
+            <div>
+                <NavBar>用户登录</NavBar>
+                <Logo/>
+                <WingBlank>
+                    <List>
+                        <InputItem placeholder='请输入用户名' onChange={(val)=>this.handlerChange('username',val)}>用户名</InputItem>
+                        <WhiteSpace/>
+                        <InputItem placeholder='请输入密码' onChange={(val)=>this.handlerChange('password',val)}>密码</InputItem>
+                        <WhiteSpace/>
+                        <Button type='primary' onClick={this.login}>登录</Button>
+                        <WhiteSpace/>
+                        <Button onClick={this.toRegister}>没有账户</Button>
+                    </List>
+
+                </WingBlank>
+            </div>
+        )
     }
 }
--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,6 +1,55 @@
 import React , {Component} from 'react';
+import {NavBar,List,InputItem,WhiteSpace,WingBlank,Button,Radio} from 'antd-mobile'
+import Logo from '../../components/Logo/logo'
 export default  class Register extends Component{
+    state={
+        username:'',
+        password:'',
+        password2:'',
+        type:'dashen'
+    };
+    handlerChange=(name,val)=>{
+        this.setState({
+            [name]:val
+        })
+    };
+    register=()=>{
+        console.log(this.state)
+
+    };
+    toLogin=()=>{
+
+        this.props.history.replace('/login')
+    }
     render(){
-        return (<div>register</div>)
+        const {type} = this.state;
+
+        return (
+            <div>
+                <NavBar>用户注册</NavBar>
+                <Logo/>
+                <WingBlank>
+                    <List>
+                        <InputItem placeholder='请输入用户名' onChange={(val)=>this.handlerChange('username',val)}>用户名</InputItem>
+                        <WhiteSpace/>
+                        <InputItem placeholder='请输入密码' onChange={(val)=>this.handlerChange('password',val)}>密码</InputItem>
+                        <WhiteSpace/>
+                        <InputItem placeholder='请输入确认密码' onChange={(val)=>this.handlerChange('password2',val)}>确认密码</InputItem>
+                        <WhiteSpace/>
+                        <List.Item>
+                            <span>用户类型:</span>
+                            <Radio checked={type=== 'dashen'} onChange={()=>this.handlerChange('type','dashen')}>大神</Radio>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio checked={type=== 'laoban'} onChange={()=>this.handlerChange('type','laoban')}>老板</Radio>
+                        </List.Item>
+                        <WhiteSpace/>
+                        <Button type='primary' onClick={this.register}>注册</Button>
+                        <WhiteSpace/>
+                        <Button onClick={this.toLogin}>已有账户</Button>
+                    </List>
+
+                </WingBlank>
+            </div>
+        )
     }
 }
