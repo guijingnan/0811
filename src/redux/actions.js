e6a06b010642bcaff4f3ad480ef4d77b8831079d
guijingnan <1023252198@qq.com>
2018-08-13 14:45:00 +0800

    update

---
+++ src/redux/actions.js
@@ -0,0 +1,37 @@
+import {AUTH_SUCCESS,ERROR_MSG} from './action-types';
+import {reqRegister,reqLogin} from "../api/index";
+/*action对象*/
+const errMsg=(msg)=>({type:ERROR_MSG,data:msg});
+const authSuccess=(user)=>({type:AUTH_SUCCESS,data:user});
+export function register({username,password,password2,type}) {
+    // 进行前台表单验证, 如果不合法返回一个同步action对象, 显示提示信息
+    if(!username || !password || !type){
+        return errMsg('用户名密码必须输入');
+    }
+    if(password !== password2){
+        return errMsg('两次密码不一致');
+    }
+    return async dispatch=>{
+        const respond = await reqRegister({username,password,type});
+        const result = respond.data;
+        if(result.code === 0){
+            dispatch(authSuccess(result.data))
+        }else{
+            dispatch(errMsg(result.msg))
+        }
+    }
+} 
+export function login({username,password}) {
+    if(!username || !password){
+        return errMsg('用户名密码必须输入');
+    }
+    return async dispatch=>{
+        const respond = await reqLogin({username,password})
+        const result = respond.data;
+        if(result.code === 0){
+            dispatch(authSuccess(result.data))
+        }else{
+            dispatch(errMsg(result.msg));
+        }
+    }
+}
